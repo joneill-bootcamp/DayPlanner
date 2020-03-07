bb51ef33c67ce05d347c4411c9d63c3fb0362092
John O'Neil <fluffywestie@tpg.com.au>
2020-03-07 23:16:37 +1100

    Added gitignore, added localstorage, cleaned up display

--- script.js
+++ script.js
@@ -12,10 +12,6 @@ $(document).ready(function () {
     } else {
         // Create a new Array of 9 elements
         var dailyPlanArray = new Array(9);
-        // Pre-populate with some values
-        dailyPlanArray[4] = "Eat a Pizza!";
-        dailyPlanArray[1] = "Start the Day";
-        dailyPlanArray[9] = "Go home!"
     }
 
     // Loop through hours in a work day
@@ -27,16 +23,16 @@ $(document).ready(function () {
         // Define and Set Display suffix default value
         var ampm = "am";
 
-        // Determine suffix based on hour of work day
+        // Determine suffix based on hour of work day for 12 hour display
         if (i <= 13) {
             ampm = "am"
         } else ampm = "pm";
 
-        // Add attributes to DIV object
+        // Add classes to DIV object
         row.addClass('row');
         row.addClass('plannerRow');
 
-        // Set custom attribute 'hour-index'
+        // Set custom attribute 'hour-index', used to facilitate array placement 
         row.attr('hour-index', i - 9);
 
         var currentTime = moment().format("H");
@@ -49,34 +45,41 @@ $(document).ready(function () {
             row.attr('display', i + " " + ampm);
         }
 
-        row.text(row.attr('display'));
+        // Use local storage 
+        if (dailyPlanArray[i - 9] == null) {
+            row.text(row.attr('display'));
+        } else {
+            row.text(dailyPlanArray[i - 9]);
+        }
 
         // Color the rows dpending on system clock time
         // NOTE, if this is runoutside business hours, all hours will be grey!!
         if (i < parseInt(currentTime)) {
-            row.css("background-color", "lightgrey");
+            row.css("background-color", "lightgrey"); // Past
         } else if (i = parseInt(currentTime)) {
-            row.css("background-color", "tomato");
+            row.css("background-color", "tomato"); // Current
         } else {
-            row.css("background-color", "lightgreen");
+            row.css("background-color", "lightgreen"); // Future
         }
 
         // Set up event for click on this row
         row.on("click", function () {
-            //alert("clicked this row" + $(this).attr("hour-index"));
+
+            // Get event details
             var eventDetails = prompt("Please enter event details");
-            //$(this).val(eventDetails);
-            //alert($(this).val());
+
+            // Set what the text for Row will be
             var textToSet = $(this).attr("display") + " : " + eventDetails;
             console.log(textToSet);
 
+            // Refresh row with it's new Text
             $(this).text(textToSet);
 
-            // Push onto array in local storage at this index for future loading
-            dailyPlanArray[parseInt($(this).attr("hour-index"))] = eventDetails;
+            // Assign element of Array to be equal to the text that Row will display 
+            dailyPlanArray[parseInt($(this).attr("hour-index"))] = textToSet;
 
-            // Update Local Storage, use Stringify to convert to text
-            //localStorage.setItem("storedArray", JSON.stringify(dailyPlanArray));
+            // Update Local Storage, use Stringify to convert array to text
+            localStorage.setItem("storedArray", JSON.stringify(dailyPlanArray));
 
         });
 
